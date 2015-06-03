aee618c369e1af0ac8c64c4dc1b202bd87d23508
Ivar Farup <ivar.farup@hig.no>
2015-06-03 21:15:19 +0200

    linting to PEP8

--- colour/statistics.py
+++ colour/statistics.py
@@ -23,17 +23,19 @@ import numpy as np
 import scipy.integrate
 import scipy.optimize
 
+
 #==============================================================================
 # Statistics of metrics
 #==============================================================================
 
+
 def stress(diff1, diff2, weights=None, confidence=.95):
     """
     Compute the STRESS for the two sets of differences.
-    
+
     The STRESS (standardised residual sum of squares) is returned as a
     percentage. If weights are given, WSTRESS is calculated.
-    
+
     Parameters
     ----------
     diff1 : ndarray
@@ -44,8 +46,9 @@ def stress(diff1, diff2, weights=None, confidence=.95):
         1D array of individual weights for the colour differences. If None,
         the standard STRESS is calculated, if given, WSTRESS is calculated.
     confidence : float
-        The size of the confidence interval (e.g., .95 for a 95% confidence interval)
-    
+        The size of the confidence interval (e.g., .95 for a 95%
+        confidence interval)
+
     Returns
     -------
     stress : float
@@ -54,13 +57,14 @@ def stress(diff1, diff2, weights=None, confidence=.95):
         The confidence interval for STRESS_a / STRESS_b
     """
     from scipy.stats import f
-    if weights == None:
+    if weights is None:
         weights = np.ones(np.shape(diff1))
     F = (diff1**2).sum() / (diff1 * diff2).sum()
-    stress = np.sqrt((weights * (diff1 - F * diff2)**2).sum() / 
+    stress = np.sqrt((weights * (diff1 - F * diff2)**2).sum() /
                      (weights * (F * diff2)**2).sum())
     N = np.shape(weights)[0]
     return stress, f.interval(confidence, N - 1, N - 1)
+
 
 def _ellipse_union(th, ell1, ell2):
     """
@@ -70,8 +74,9 @@ def _ellipse_union(th, ell1, ell2):
                               ell1[0]**2 * np.sin(th-ell1[2])**2))
     r2 = ell2[0] * ell2[1] / (np.sqrt(ell2[1]**2 * np.cos(th-ell2[2])**2 +
                               ell2[0]**2 * np.sin(th-ell2[2])**2))
-    u = max(r1,r2)
+    u = max(r1, r2)
     return .5 * u**2
+
 
 def _ellipse_intersection(th, ell1, ell2):
     """
@@ -81,30 +86,33 @@ def _ellipse_intersection(th, ell1, ell2):
                               ell1[0]**2 * np.sin(th-ell1[2])**2))
     r2 = ell2[0] * ell2[1] / (np.sqrt(ell2[1]**2 * np.cos(th-ell2[2])**2 +
                               ell2[0]**2 * np.sin(th-ell2[2])**2))
-    u = min(r1,r2)
+    u = min(r1, r2)
     return .5 * u**2
+
 
 def _pant_R_value(ell1, ell2):
     """
     Compute single R value for the two given ellipses.
-    """    
+    """
     area_intersection = scipy.integrate.quad(_ellipse_intersection,
-                                             0, 2 * np.pi, (ell1, ell2))    
+                                             0, 2 * np.pi, (ell1, ell2))
     area_union = scipy.integrate.quad(_ellipse_union,
                                       0, 2 * np.pi, (ell1, ell2))
     return area_intersection[0] / area_union[0]
+
 
 def _pant_R_values(ells1, ells2, scale=1):
     """
     Compute set of R values for the two given sets of ellipses.
     """
     ells1 = ells1.copy()
-    ells1[:,0:2] = ells1[:,0:2] * scale
+    ells1[:, 0:2] = ells1[:, 0:2] * scale
     N = np.shape(ells1)[0]
     r_values = np.zeros(N)
     for i in range(N):
         r_values[i] = _pant_R_value(ells1[i], ells2[i])
     return r_values
+
 
 def _cost_function_pant(scale, ells1, ells2):
     """
@@ -112,17 +120,18 @@ def _cost_function_pant(scale, ells1, ells2):
     """
     r_values = _pant_R_values(ells1, ells2, scale)
     return 1 - r_values.mean()
-    
+
+
 def pant_R_values(space, tdata1, tdata2, optimise=True, plane=None):
     """
     Compute the list of R values for the given metric tensors in tdataN.
-    
+
     The R values are computed in the given colour space. If optimise=True,
     the maximum overall R values are found by scaling one of the data sets.
     The ellipses are computed in the given plane. If plane=None, all three
     principal planes are used, and the resulting array of R values will
     be three times the length ot tdataN.
-    
+
     Parameters
     ----------
     space : Space
@@ -135,13 +144,13 @@ def pant_R_values(space, tdata1, tdata2, optimise=True, plane=None):
         Whether or not to optimise the scaling of the ellipse set.
     plane : slice
         The principal plan for the ellipsoid cross sections.
-    
+
     Returns
     -------
     r_values : ndarray
         Pant R values
-    """    
-    if plane == None:
+    """
+    if plane is None:
         ell1a = tdata1.get_ellipse_parameters(space, tdata1.plane_01)
         ell1b = tdata1.get_ellipse_parameters(space, tdata1.plane_12)
         ell1c = tdata1.get_ellipse_parameters(space, tdata1.plane_20)
@@ -157,19 +166,20 @@ def pant_R_values(space, tdata1, tdata2, optimise=True, plane=None):
         res = scipy.optimize.fmin(_cost_function_pant, 1, (ell1, ell2))
         return _pant_R_values(ell1, ell2, res[0]), res[0]
     else:
-        return _pant_R_values(ell1, ell2)        
+        return _pant_R_values(ell1, ell2)
+
 
 def dataset_distance(data1, data2):
     """
     Euclidean distances between data in the two data sets.
-    
+
     Parameters
     ----------
     data1 : ndarray
         The first dataset, Nx3.
     data2 : ndarray
         The second data set, Nx3.
-    
+
     Returns
     -------
     diff : ndarray
@@ -177,10 +187,11 @@ def dataset_distance(data1, data2):
     """
     return np.sqrt(((data1 - data2)**2).sum(axis=1))
 
+
 def _scale_rot_dataset(params, dataset):
     """
     Scale and rotate dataset for optimisation.
-    
+
     Parameters
     ----------
     params : ndarray
@@ -194,7 +205,7 @@ def _scale_rot_dataset(params, dataset):
         The scaled and rotated dataset.
     """
     scale_mat = params[1] * np.eye(3)
-    scale_mat[0,0] = params[0]
+    scale_mat[0, 0] = params[0]
     th = params[2]
     rot_mat = np.array([[1, 0, 0],
                         [0, np.cos(th), -np.sin(th)],
@@ -202,10 +213,11 @@ def _scale_rot_dataset(params, dataset):
     sys_mat = np.dot(scale_mat, rot_mat)
     return np.dot(sys_mat, dataset.T).T
 
+
 def _cost_function_dataset(params, dataset, ground_truth):
     """
     Cost function for the optimisation of the scale for the R values.
-    
+
     Parameters
     ----------
     params : ndarray
@@ -214,28 +226,30 @@ def _cost_function_dataset(params, dataset, ground_truth):
         The data set to optimise.
     ground_truth : ndarray
         The ground truth dataset.
-    
+
     Returns
     -------
     cost : float
         The cost for the current values of scale and angle.
     """
-    return dataset_distance(_scale_rot_dataset(params, dataset), ground_truth).sum()
+    return dataset_distance(
+        _scale_rot_dataset(params, dataset), ground_truth).sum()
+
 
 def minimal_dataset_distance(dataset, ground_truth):
     """
     Return the minimal dataset distance between a dataset and a ground truth.
-    
+
     The dataset is assumed to be on the Lab form (as an Nx3 ndarray) and is
     changed by scaling and rotation about the L axis.
-    
+
     Parameters
     ----------
     dataset : ndarray
         Nx3 ndarray with the colour data.
     ground_truth : ndarray
         Nx3 ndarray with the ground truth colour data.
-    
+
     Returns
     -------
     diff : ndarray
@@ -249,13 +263,17 @@ def minimal_dataset_distance(dataset, ground_truth):
     angle : float
         The optimal angle.
     """
-    params = scipy.optimize.fmin(_cost_function_dataset, np.array([1,1,0]), (dataset, ground_truth))
+    params = scipy.optimize.fmin(_cost_function_dataset, np.array([1, 1, 0]),
+                                 (dataset, ground_truth))
     opt_data = _scale_rot_dataset(params, dataset)
-    return dataset_distance(opt_data, ground_truth), opt_data, params[0], params[1], params[2]
+    return dataset_distance(opt_data, ground_truth), \
+        opt_data, params[0], params[1], params[2]
+
 
 #==============================================================================
 # Test module
 #==============================================================================
+
 
 def test():
     """
