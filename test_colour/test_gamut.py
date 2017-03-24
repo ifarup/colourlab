473da510fafecd529df2e892ee8bddf5b42d46a5
Lars Niebuhr <larsmni@stud.ntnu.no>
2017-03-24 09:14:45 +0100

    PGL-58 pep8

--- test_colour/test_gamut.py
+++ test_colour/test_gamut.py
@@ -450,7 +450,7 @@ class TestGamut(unittest.TestCase):
         c_data = data.Data(space.srgb, points)                              # data.Data object
         re_data = g.intersectionpoint_on_line(space.srgb, c_data)           # data.Data object returned
 
-        self.assertTrue(np.allclose(re_data.get_linear(space.srgb), mod_points))    # assert that the points are changed
+        self.assertTrue(np.allclose(re_data.get_linear(space.srgb), mod_points))  # assert that the points are changed
 
 if __name__ == '__main__':
     unittest.main(exit=False)
