a572b0fb40791ed7dcd743e896aff297d915ab2e
Ivar Farup <ivar.farup@ntnu.no>
2017-05-24 19:42:31 +0200

    #17 trying to connect to readthedocs.org

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,7 @@ extensions = [
     'sphinx.ext.coverage',
     'sphinx.ext.mathjax',
     'sphinx.ext.viewcode',
-    'numpydoc'
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
