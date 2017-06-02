d410e31d72613dbb78218771ef32597fb00a53d5
Ivar Farup <ivar.farup@ntnu.no>
2017-06-02 14:23:28 +0200

    version 0.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="colourlab",
-    version="0.1.0.dev",
+    version="0.0.1",
     packages=['colourlab'],
     include_package_data=True,
     package_data={'colourlab': ['colour_data/*',
