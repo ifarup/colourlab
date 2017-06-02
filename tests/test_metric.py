aa10e8c9e899140e1ac3568b421c28f9260e0b03
Ivar Farup <ivar.farup@ntnu.no>
2017-06-02 11:46:21 +0200

    #23 added tests of metrics

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -21,3 +21,20 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import unittest
+import numpy as np
+from colourspace import metric, data, space
+
+d1 = data.d_regular(space.cielab,
+                    np.linspace(20, 80, 10),
+                    np.linspace(-50, 50, 11),
+                    np.linspace(-50, 50, 11))
+d2 = data.Points(space.cielab,
+                 d1.get(space.cielab) + 1 / np.sqrt(3))
+
+class TestMetrics(unittest.TestCase):
+    def test_metrics(self):
+        for met in [metric.dE_ab, metric.dE_uv,
+                    metric.dE_00, metric.dE_DIN99,
+                    metric.dE_DIN99b, metric.dE_DIN99c,
+                    metric.dE_DIN99d]:
+            self.assertTrue(np.max(met(d1, d2) < 5))
