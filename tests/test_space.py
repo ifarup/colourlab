ac21408c85b79641914b66e45e0607dfc4c6bcd2
Ivar Farup <ivar.farup@ntnu.no>
2017-06-06 22:08:51 +0200

    #25 tests for predefined colour spaces

--- tests/test_space.py
+++ tests/test_space.py
@@ -20,3 +20,55 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 """
 
 import unittest
+import numpy as np
+from colourlab import space, data
+
+# Spaces and data for testing
+
+_test_ui = space.TransformLinear(
+    space.TransformGamma(
+        space.TransformLinear(
+            space.xyz,
+            np.array([[0.1551646, 0.5430763, -0.0370161],
+                      [-0.1551646, 0.4569237, 0.0296946],
+                      [0, 0, 0.0073215]])), .43),
+    np.array([[1.1032e+00, 5.0900e-01, 5.0840e-03],
+              [2.2822e+00, -4.2580e+00, 6.2844e+00],
+              [9.6110e+00, -1.2199e+01, -2.3843e+00]]))
+_test_space_cartesian = space.TransformCartesian(space.cieluv)
+_test_space_poincare_disk = space.TransformPoincareDisk(space.cielab)
+_test_space_gamma = space.TransformGamma(space.xyz, .43)
+
+col = np.array([[1e-10, 1e-10, 1e-10], [.95, 1., 1.08], [.5, .5, .5]])
+
+
+class TestSpace(unittest.TestCase):
+
+    def test_predefined_space_transforms(self):
+        test_spaces = [space.xyz, space.xyY, space.cielab, space.cieluv,
+                       space.cielch, space.ipt,
+                       space.din99, space.din99b, space.din99c, space.din99d,
+                       _test_space_cartesian, _test_space_poincare_disk,
+                       _test_space_gamma]
+        for sp in test_spaces:
+            c2 = sp.to_XYZ(sp.from_XYZ(col))
+            err = np.max(np.abs(col - c2))
+            self.assertTrue(err < .1)
+
+    def test_predefined_space_jacobians(self):
+        col_data = data.Points(space.xyz, col)
+        test_spaces = [space.xyz, space.xyY, space.cielab,
+                       space.cieluv, space.cielch, space.ipt,
+                       space.ciede00lab, space.din99, space.din99b,
+                       space.din99c, space.din99d,
+                       _test_space_cartesian,
+                       _test_space_poincare_disk, _test_space_gamma]
+        for sp in test_spaces:
+            jac1 = sp.jacobian_XYZ(col_data)
+            jac2 = sp.inv_jacobian_XYZ(col_data)
+            prod = np.zeros(np.shape(jac1))
+            for i in range(np.shape(jac1)[0]):
+                prod[i] = np.dot(jac1[i], jac2[i])
+                prod[i] = np.abs(prod[i] - np.eye(3))
+            err = np.max(prod)
+            self.assertTrue(err < 1e-6)
